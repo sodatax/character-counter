e14a10f2e9d7819211c5b8e300fc3bf73ca2ada7
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-04-16 11:12:59 -0700

    added test cases and fixed bug in percent frequency

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,25 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+    @Test
+    void testCharacterShowsUpMultipleTimesGivesCorrectFrequency(){
+        //Arange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+        
+        //Act
+        int actual = counter.getFrequency('l');
+
+        //Assert
+        assertEquals(2, actual);
+    }
+
+    @Test
+    void testGetRelativeFrequencyReturns0ForMissingCharacter(){
+        //arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+        //act
+        double actual = counter.getRelativePercentage('x');
+        //assert
+        assertEquals(0.0, actual, 0.0001);
+    }
 }
